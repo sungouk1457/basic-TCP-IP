4d2625b790bd013902fed799907cc6714756855f
sungouk1457 <sungouk1457@naver.com>
2024-06-19 15:28:17 +0900

    test

--- day07/webserv2.c
+++ day07/webserv2.c
@@ -16,7 +16,7 @@ char webpage[]="HTTP/1.1 200 OK\r\n"
      					 "Content-Type: text/html; charset=UTF-8\r\n\r\n"
      					 "<!DOCTYPE html>\r\n"
      					 "<html><head><title> My Web Page </title>\r\n"
-     					 "<style>body {background-color: skyblue}</style></head>\r\n"
+     					 "<style>body {background-color:#7fd2f0 }</style></head>\r\n"
      					 "<body><center><h1>Hello World</h1><br>\r\n"
      					 "<img src=\"cat.jpg\"></center></body></html>\r\n";
 int main(int argc, char *argv[])
